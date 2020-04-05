ea15b8ca89b2b149f4e0f093260ab5e79e1bfd49
John Moore <jmoore@bluestatedigital.com>
2020-04-05 16:46:39 -0700

    Import vue theme

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php status_header(200); ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+
+  <head>
+    <meta
+      charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page page--home">
+    <h1>The Hotdrop</h1>
+    <recent-posts-widget limit="5" class="mb-10">Recent Posts</recent-posts-widget>
+    <pages-widget limit="5">Pages</pages-widget>
+  </div>
+</template>
+
+<script>
+import RecentPostsWidget from './widgets/RecentPosts.vue';
+import PagesWidget from './widgets/Pages.vue';
+
+export default {
+  components: {
+    RecentPostsWidget,
+    PagesWidget,
+  },
+};
+</script>
+
+<style type="postcss"></style>
